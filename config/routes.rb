440dbf6e20efcbc917bf40accba7246855be9d9b
Yunqiao Yang <yunqiaoyang@Yunqiaos-MacBook-Pro.local>
2018-11-11 20:34:18 -0500

    add request SSM frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #pages controller, home action
+  root 'pages#home'
+
+  # pages controller, about action
+  get 'ssm', to: 'pages#ssm'
+
   resources :courses
   resources :schools
   resources :students
